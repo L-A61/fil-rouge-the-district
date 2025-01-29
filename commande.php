a8acd756a3f28a84f2f8284e3653193b836ac385
Axel <schoolandaxel61@gmail.com>
2025-01-29 10:47:39 +0100

    Faire une première commande ajoute les informations d'un client dans la table client

--- commande.php
+++ commande.php
@@ -1,32 +1,42 @@
 <?php
 include './header.php';
 
+$utilisateur = $_SESSION['utilisateur_ID'] ?? null;
+
+$clientExistant = null;
+$adressesExistantes = [];
+
+if ($utilisateur) {
+    $stmtClient = $pdo->prepare("SELECT * FROM client WHERE utilisateur_ID = :utilisateur_ID");
+    $stmtClient->execute([':utilisateur_ID' => $_SESSION['utilisateur_ID']]);
+    $clientExistant = $stmtClient->fetch(PDO::FETCH_ASSOC);
+}
+
+
 // Récupération des informations du client
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Récupération des données du formulaire en méthode POST
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $email = $_POST['email'];
-    $tel = $_POST['tel'];
-    $adresse = $_POST['adresse'];
-    $adresse2 = $_POST['adresse2'];
-    $adresse3 = $_POST['adresse3'];
-    $codepostal = $_POST['codepostal'];
-    $ville = $_POST['ville'];
+    $nom = $_POST['nom'] ?? null;
+    $prenom = $_POST['prenom'] ?? null;
+    $tel = $_POST['tel'] ?? null;
+    $adresse = $_POST['adresse'] ?? null;
+    $adresse2 = $_POST['adresse2'] ?? null;
+    $adresse3 = $_POST['adresse3'] ?? null;
+    $codepostal = $_POST['codepostal'] ?? null;
+    $ville = $_POST['ville'] ?? null;
+
+    $commandeID = 0;
+
+    if (!$clientExistant) {
+        $stmt = $pdo->prepare("INSERT INTO client (client_nom, client_prenom, client_tel, client_cp, client_ville, client_adresse1, client_adresse2, client_adresse3, utilisateur_ID) 
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->execute([$nom, $prenom, $tel, $adresse, $adresse2, $adresse3, $codepostal, $ville, $utilisateur]);
+        $clientID = $pdo->lastInsertId();
+    }
 
 
     // Insertion de l'utilisateur dans la base de données (id utilisateur à ajouter plus tard)
-    $stmt = $pdo->prepare("INSERT INTO client (client_nom, client_prenom, client_tel, client_cp, client_ville, client_adresse1, client_adresse2, client_adresse3) 
-		VALUES (:client_nom, :client_prenom, :client_tel, :client_cp, :client_ville, :client_adresse1, :client_adresse2, :client_adresse3)");
-    $stmt->bindValue(':client_nom', $nom);
-    $stmt->bindValue(':client_prenom', $prenom);
-    $stmt->bindValue(':client_tel', $tel);
-    $stmt->bindValue(':client_cp', $codepostal);
-    $stmt->bindValue(':client_ville', $ville);
-    $stmt->bindValue(':client_adresse1', $adresse);
-    $stmt->bindValue(':client_adresse2', $adresse2);
-    $stmt->bindValue(':client_adresse3', $adresse3);
-    $stmt->execute();
+    
 
     header('Location: index.php'); // Redirection vers la page d'accueil
     exit();
@@ -37,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <main>
     <section>
 
-        <h1>Votre Commande</h1>
+        <h1>Votre Commande <?= "Login: ".$utilisateur;?></h1>
 
         <!-- Aperçu commande -->
 
@@ -52,11 +62,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <article>
                 <label for="prenom">Prénom:</label>
                 <input id="prenom" type="text" name="prenom">
-            </article>
-
-            <article>
-                <label for="email">Email:</label>
-                <input id="email" type="text" name="email">
             </article>
 
             <article>
